bddb0c2183aac4d8ac1b1df34017328164acf740
David <47631890+StudDavid@users.noreply.github.com>
2020-08-13 15:58:26 +0200

    reformatting of code rewrote spit function for more readability

--- src/bacteria.py
+++ src/bacteria.py
@@ -9,6 +9,7 @@ This is a awesome
 # imports
 import random
 import math
+import numpy as np
 
 
 # ********************************************************************************************
@@ -24,7 +25,7 @@ def absolute(value):
 # main class <<<bacterium>>>
 # the structure corresponds now to previous versions
 
-class bacterium(object):
+class Bacterium(object):
 
     def __init__(self, pos, width, length, velocity, angle, totalForce_equivalent, living, moving):
         """
@@ -49,95 +50,66 @@ class bacterium(object):
         self.living = living
         self.moving = moving
 
-    def print_info(self):
-        """
-        print all values of parameters of bacteria
-        :return:
-        """
-        print("\n**** Current values of bacci {index} ****\n"
-              "  Position       ({x_pos:f}, {y_pos:f})\n"
-              "  Width, Length      {width:.2f},{length:.2f}\n"
-              "  angle            {o:.1f}\n"
-              "  velocity           {vx:.2f}, {vy:.2f}".format(index=self.index, x_pos=self.pos[0],
-                                                               y_pos=self.pos[1],
-                                                               width=self.width, length=self.length,
-                                                               o=self.angle, vx=self.v_x, vy=self.v_y)
-              )
-
-    def grow(self, gr_factor, gr_pr_i, gr_factor_inv, gr_d_factor, mortility_rate):
+    def grow(self, gr_factor, gr_pr_i, gr_factor_inv, gr_d_factor, mortality_rate):
         """
         grow bacteria for 1 second with speed growth_rate
         """
         # Make the bacteria grow
         # using a constant growth rate ( volume per time)
         # growths inhibition factor
-        if (self.living == True):
-            if (self.moving == False):
+        if self.living:
+            if self.moving:
                 growth_suppressor = gr_factor * gr_pr_i / (gr_pr_i + self.totalForce_equivalent * 0.5) - gr_factor_inv
                 volume = self.getVolume()
                 growth_factor = (volume + volume ** (1 / 3) * 5.0 * growth_suppressor * growth_suppressor) / volume
-                # self.width  = self.width *growth_factor
                 self.length = self.length * growth_factor
         else:
-            # self.width  = self.width *gr_d_factor
             self.length = self.length * gr_d_factor
 
         # Programmed cell death
-        if (random.random() > 1.0 - mortility_rate):
+        if random.random() > 1.0 - mortality_rate:
             self.living = False
 
     def split(self):
         """
-        split bacteria if critical length is reached, create new bacteria with new values
+        split bacteria  create new daughter bacteria with new values and update the mother bacterium
         :return: daughter bacteria
         """
-        volumeRatio = 0.4 + 0.2 * random.random()
-        volume = volumeRatio * self.getVolume()
-        # Additional growth loss
-        # ------looks better-------
-        # if(True):
-        # volume = volume*0.8*0.8*0.8
-
+        # Calculate new position and angle
         # Advanced new position: random angular component,
         #                       radial component sum of two radii*0.8
-        spawn_angle = [self.angle[0] + (0.5 - random.random()) * 3.1415 * 0.5,
-                       self.angle[1] + (0.5 - random.random()) * 3.1415 * 0.5]
-        self.angle = [self.angle[0] + (0.5 - random.random()) * 3.1415 * 0.5,
-                      self.angle[1] + (0.5 - random.random()) * 3.1415 * 0.1]
-        # Small probability: also rotate cell orientation by 180°
-        if (random.random() > 0.8):
-            spawn_angle[0] = spawn_angle[0] + 3.1415
-        if (random.random() > 0.8):
-            self.angle[0] = self.angle[0] + 3.1415
-        # spawn_distance = (self.getVolume())**(1/3)*0.75
-        spawn_distance = (self.length) * 1.0 * 0.1
-        # print(self.getVolume())
-        # print(spawn_distance)
-        spawn_offsetX = spawn_distance * math.sin(spawn_angle[0]) * math.cos(spawn_angle[1])
-        spawn_offsetY = spawn_distance * math.cos(spawn_angle[0]) * math.cos(spawn_angle[1])
-        spawn_offsetZ = spawn_distance * math.sin(spawn_angle[1])
-        # Move cell with the ratio 1/mass
-        # massFactor = (self.getVolume()-volume)/(self.getVolume())
-        pos = (self.pos[0] + spawn_offsetX * volumeRatio, self.pos[1] + spawn_offsetY * volumeRatio,
-               self.pos[2] + spawn_offsetZ * volumeRatio)
-        # print(str(self.pos) + " " + str((spawn_offsetX, massFactor, spawn_offsetY)))
-        # bac = self(pos, (0,0,0), volume, 0)
-        # width  = self.width#0.5*volume**(1/3)
-        length = volumeRatio * self.length  # (volume*volumeRatio)**(2/3)/width
-        bac = bacterium(pos, self.width, length, (0, 0, 0), spawn_angle, 0, True, False)
-        # print(str(pos) +" "+ str(width) +" "+ str(length) +" "+ str((0,0,0)) +" "+ str(random.random()*3.14))
+        def update_angle(angle):
+            angle = [angle[0] + (0.5 - random.random()) * np.pi * 0.5,
+                     angle[1] + (0.5 - random.random()) * np.pi * 0.5]
+            return angle
 
-        # Also modify parent self
-        # self.width  = self.width #*(1-volumeRatio)**(1/3)*0.8#growth_factor
-        # self.length = self.length*(1-volumeRatio)**(1/3)#*growth_factor
-        self.length = (1 - volumeRatio) * self.length  # (volume*(1-volumeRatio))**(2/3)/width
+        def get_daughter_position(position, split_distance, angle):
+            offset = (split_distance * math.sin(angle[0]) * math.cos(angle[1]),
+                      split_distance * math.cos(angle[0]) * math.cos(angle[1]),
+                      split_distance * math.sin(angle[1]))
 
-        self.pos[0] = self.pos[0] - spawn_offsetX * (1.0 - volumeRatio)
-        self.pos[1] = self.pos[1] - spawn_offsetY * (1.0 - volumeRatio)
-        self.pos[2] = self.pos[2] - spawn_offsetZ * (1.0 - volumeRatio)
+            position = (position[0] + offset[0] * volumeRatio,
+                        position[1] + offset[1] * volumeRatio,
+                        position[2] + offset[2] * volumeRatio)
+            return position, offset
+
+        volumeRatio = 0.4 + 0.2 * random.random()
+        # Create daughter bacterium from self
+        daughter_bac = Bacterium(self.pos, self.width, self.length, (0, 0, 0), self.angle, 0, True, False)
+        # Update parameters of daughter and mother bacterium
+        daughter_bac.length = volumeRatio * self.length
+        daughter_bac.angle = update_angle(self.angle)
+        daughter_bac.pos, spawn_offset = get_daughter_position(position=self.pos, split_distance=self.length * 0.3,
+                                                               angle=daughter_bac.angle)
+        self.length = (1 - volumeRatio) * self.length
+
+        # self.angle = update_angle(self.angle)
+        # self.pos[0] = self.pos[0] - spawn_offset[0] * (1.0 - volumeRatio)
+        # self.pos[1] = self.pos[1] - spawn_offset[1] * (1.0 - volumeRatio)
+        # self.pos[2] = self.pos[2] - spawn_offset[2] * (1.0 - volumeRatio)
 
         # return new bacterium
-        return bac
+        return daughter_bac
 
     def getPositions(self):
         positions = []
@@ -181,18 +153,18 @@ class bacterium(object):
 
         # Boundary collision
         for position in self.getPositions():
-            if (False):
+            if False:
                 xborder = frameDim[1] * 0.5
-                if (position[0] < -xborder + self.width):
+                if position[0] < -xborder + self.width:
                     self.velocity[0] = self.velocity[0] + (-position[0] - xborder + self.width) * 0.1
                     self.totalForce_equivalent = self.totalForce_equivalent + absolute(
                         -position[0] - xborder + self.width) * 0.1
-                if (position[0] > xborder - self.width):
+                if position[0] > xborder - self.width:
                     self.velocity[0] = self.velocity[0] + (-position[0] + xborder - self.width) * 0.1
                     self.totalForce_equivalent = self.totalForce_equivalent + absolute(
                         -position[0] + xborder - self.width) * 0.1
             yborder = frameDim[0] * 0.5
-            if (position[1] < -yborder + self.width):
+            if position[1] < -yborder + self.width:
                 self.velocity[1] = self.velocity[1] + (-position[1] - yborder + self.width) * 0.1
                 self.totalForce_equivalent = self.totalForce_equivalent + absolute(
                     -position[1] - yborder + self.width) * 0.1
@@ -200,7 +172,7 @@ class bacterium(object):
             #        self.velocity[1] = self.velocity[1] + (-position[1]+yborder-self.width)*0.1    
             #        self.totalForce_equivalent = self.totalForce_equivalent + absolute(-position[1]+yborder-self.width)*0.1    
             # Bottom-boundary (z<=0)
-            if (position[2] < -0 + self.width):
+            if position[2] < -0 + self.width:
                 self.velocity[2] = self.velocity[2] + (-position[2] + self.width) * 0.1
                 # self.totalForce_equivalent = self.totalForce_equivalent + absolute(-position[2]+self.width)*0.1
 
@@ -314,8 +286,8 @@ class bacterium(object):
                                 self.angle[1]) * repulsion_y / lenPos * 0.05 * interactionfactor
                             # Torque on second angle
                             self.velocity_angular[1] = self.velocity_angular[1] + t_radius * math.cos(self.angle[1]) * (
-                                        repulsion_x ** 2 + repulsion_y ** 2) ** (
-                                                                   1 / 2) / lenPos * 0.0125 * interactionfactor
+                                    repulsion_x ** 2 + repulsion_y ** 2) ** (
+                                                               1 / 2) / lenPos * 0.0125 * interactionfactor
                             self.velocity_angular[1] = self.velocity_angular[1] + t_radius * math.sin(
                                 self.angle[1]) * repulsion_z / lenPos * 0.05 * interactionfactor
                             self.totalForce_equivalent = self.totalForce_equivalent + absolute(
@@ -325,35 +297,14 @@ class bacterium(object):
 
                             # Actio-Reactio
                             _Bacterium.velocity[0] = _Bacterium.velocity[0] - (
-                                        repulsion_x / lenPos ** (1 / 2) * interactionfactor)
+                                    repulsion_x / lenPos ** (1 / 2) * interactionfactor)
                             _Bacterium.velocity[1] = _Bacterium.velocity[1] - (
-                                        repulsion_y / lenPos ** (1 / 2) * interactionfactor)
+                                    repulsion_y / lenPos ** (1 / 2) * interactionfactor)
                             _Bacterium.velocity[2] = _Bacterium.velocity[2] - (
-                                        repulsion_z / lenPos ** (1 / 2) * interactionfactor)
+                                    repulsion_z / lenPos ** (1 / 2) * interactionfactor)
                             _Bacterium.totalForce_equivalent = _Bacterium.totalForce_equivalent + absolute(
                                 repulsion_y / lenPos * interactionfactor)
                             _Bacterium.totalForce_equivalent = _Bacterium.totalForce_equivalent + absolute(
                                 repulsion_x / lenPos * interactionfactor)
 
         return _Bacterium, self
-
-    # Actual not used structural part due to the interaction part
-    # and resulting design difficulties
-    # see core function
-    '''
-    def evolution(self, duration):
-        """ evolution of cell for duration, break evolution if new cell is created  """
-        iterations = []
-        bacteria = [self]
-        for t in range(0, duration):
-            for bac in bacteria:
-                bac.move()
-                bac.grow()
-                #if bac.length >= bac.split_length:
-                #    bac, new_bac = bac.split()
-                #    bacteria.extend((bac, new_bac))
-
-            iterations.append(bacteria)
-
-        return iterations
-    '''

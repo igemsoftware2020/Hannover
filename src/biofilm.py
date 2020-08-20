11b43dcc26288209e61010450df0e16b600c5690
David <47631890+StudDavid@users.noreply.github.com>
2020-08-20 17:56:08 +0200

    updated functions to use constants changed the interaction function

--- src/biofilm.py
+++ src/biofilm.py
@@ -1,4 +1,5 @@
-# coding latin-1
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
 # ********************************************************************************************
@@ -8,6 +9,7 @@ import random
 
 # custom libraries
 from src.bacteria import Bacterium
+import src.constants as c
 
 
 class Biofilm(object):
@@ -15,63 +17,71 @@ class Biofilm(object):
     def __init__(self):
         self.bacteria = []
 
-    def spawn(self, startNum):
-        for _ in range(startNum):
-            pos = ((random.random() - 0.5) * 400.0, (random.random() - 0.5) * 400.0, 0)
-            # volume = 3300+1700*random.random()
-            width = 0.4 * 15  # *volume**(1/3)
-            # width  = 0.4 *15#*volume**(1/3)
-            length = 4 * 8 * 15  # *volume**(1/3)
-            bac = Bacterium(pos, width, length, (0, 0, 0), [random.random() * 3.14, (0.5 - random.random() * 3.14)], 0,
-                            True, False)
+    def spawn(self):
+        for _ in range(c.START_NUMBER_BACTERIA):
+            pos = np.asarray(
+                [(random.random() - 0.5) * c.WINDOW_SIZE[0], (random.random() - 0.5) * c.WINDOW_SIZE[1], 0])
+            velocity = np.asarray([random.gauss(c.BSUB_MEAN_SPEED, 10), random.gauss(c.BSUB_MEAN_SPEED, 10), 0])
+            bac = Bacterium(position=pos, velocity=velocity)
             self.bacteria.append(bac)
 
-    def print_current_state(self):
-        print("***** Current state of Biofilm ******\n"
-              "     Bacteria alive : {alive:.0f}\n"
-              "     ".format(bacteria=len(self.bacteria))
-              )
+    @staticmethod
+    def check_energy_conservation(bacterium1: Bacterium, bacterium2: Bacterium, total_energy_before):
+        if bacterium1.total_energy + bacterium2.total_energy != total_energy_before:
+            raise ValueError("Energy conversation broken while splitting.")
 
-    def evolve(self, gr_factor, gr_pr_i, gr_factor_inv, gr_d_factor, mortality_rate, motion_activation,
-               motion_deactivation, frameDim, combined_friction, splitting_rate, dt):
-        for Bacterium in self.bacteria:
-            Bacterium.grow(gr_factor, gr_pr_i, gr_factor_inv, gr_d_factor, mortality_rate)
+    def evolve(self):
+        for bacterium in self.bacteria:
+            bacterium.grow()
 
-            if (Bacterium.living == False) and (Bacterium.getVolume() < 3300):
-                self.bacteria.remove(Bacterium)
+            if not bacterium.living and bacterium.getVolume() < 3300:
+                self.bacteria.remove(bacterium)
             # Manage repulsion
             # (The most expensive task)
-            for _Bacterium in self.bacteria:
-                if ((Bacterium.pos[0] == _Bacterium.pos[0]) and (Bacterium.pos[1] == _Bacterium.pos[1]) and (
-                        Bacterium.pos[2] == _Bacterium.pos[2])):
-                    True
-                else:
-                    [_Bacterium, Bacterium] = Bacterium.interaction(_Bacterium)
+            for _bacterium in self.bacteria:
+                [_bacterium, bacterium] = Biofilm.interaction(bacterium, _bacterium)
 
-            Bacterium.move(frameDim, combined_friction, dt)
+            bacterium.move()
 
             # Manage Bacterial splitting
             # Add a little bit of random until -----it looks good and real-----
-            if (Bacterium.getVolume() > 20000) and (Bacterium.living) and (Bacterium.moving == False):
-                if (random.random() > splitting_rate) or (Bacterium.getVolume() > 35000):
-                    # Split Bacterium into two,
-                    # Differate between old and new cell
-                    # Now add new bacterium to
-                    self.bacteria.append(Bacterium.split())
+            if bacterium.is_split_ready() and bacterium.living:
+                energy_before = bacterium.total_energy
+                daughter = bacterium.split()
+                self.check_energy_conservation(bacterium, daughter, energy_before)
+                self.bacteria.append(daughter)
+
             # Manage Bacterial Motion-Mode
             # Enable/Disable motion mode
-            if (Bacterium.getVolume() > 20000) and (Bacterium.living):
-                if (random.random() > 1.0 - motion_activation):
-                    Bacterium.moving = True
-                if (random.random() > 1.0 - motion_deactivation):
-                    Bacterium.moving = False
+            if bacterium.living:
+                if random.random() > 1.0 - c.MOTION_ACTIVATION_PROBABILITY:
+                    bacterium.moving = True
+                if random.random() > 1.0 - c.MOTION_DEACTIVATION_PROBABILITY:
+                    bacterium.moving = False
             else:
-                Bacterium.moving = False
+                bacterium.moving = False
 
-    # This function should be removed here and included in core library
-    # If using OpenGL with depth sorting enabled,
-    # This function also gets redundant
-    def sortbydepth(self, axis, _reverse):
+    @staticmethod
+    def interaction(bacterium1: Bacterium, bacterium2: Bacterium):
+        """ return interaction term with bacteria in local environment"""
+
+        def lennard_jones_force(r, epsilon, r_min):
+            return - epsilon * (12 * (r_min ** 12 / r ** 13) - 12 * (r_min ** 6 / r ** 7))
+
+        distance_vector = bacterium1.position - bacterium2.position
+        distance = np.sqrt(np.dot(distance_vector, distance_vector))
+
+        if distance < bacterium1.length * 4 and distance != 0:
+            # Values for epsilon and r_min kinda random
+            repulsive_force = lennard_jones_force(distance, epsilon=0.1, r_min=bacterium1.length)
+            acceleration = repulsive_force / c.BSUB_MASS
+            def new_position(a, v, t, r): return 1 / 2 * a * t ** 2 + v * t + r
+            bacterium1.position = new_position(acceleration, bacterium1.velocity, c.TIME_STEP, bacterium1.position)
+            bacterium2.position = new_position(acceleration, bacterium2.velocity, c.TIME_STEP, bacterium2.position)
+
+        return bacterium1, bacterium2
+
+    def sort_by_depth(self, axis, _reverse):
         sorted_bacteria = self.bacteria
         # To return a new list, use the sorted() built-in function...
-        return sorted(sorted_bacteria, key=lambda x: x.pos[axis], reverse=_reverse)
+        return sorted(sorted_bacteria, key=lambda x: x.position[axis], reverse=_reverse)

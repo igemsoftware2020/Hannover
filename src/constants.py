e2a476d644786e4d5a392621163dc2081397c90f
David <47631890+StudDavid@users.noreply.github.com>
2020-08-20 17:52:29 +0200

    introduced new constants for Bactilius subtilis renamed constants

---
+++ src/constants.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# SIMULATION PARAMETERS
+TIME_STEP = 40  # in seconds
+
+
+# constants B. subtilius
+
+BSUB_WIDTH = 1    # MICROMETERS https://en.wikipedia.org/wiki/Bacillus_subtilis
+BSUB_LENGTH = 4  # MICROMETERS https://en.wikipedia.org/wiki/Bacillus_subtilis
+BSUB_MASS = 10 ** (-15)  # kg
+BSUB_MORTALITY_RATE = 0.06
+BSUB_CRITICAL_LENGTH = 4.7  # MICROMETERS
+BSUB_MEAN_SPEED = 50  # 50 MICROMETERS / s mean speed
+
+BSUB_DOUBLING_TIME = 720    # SECONDS DOI: 10.1128/jb.167.1.219-230.1986
+BSUB_GROWTH_FACTOR = 2 * BSUB_LENGTH / BSUB_DOUBLING_TIME  # MICROMETERS / s
+
+
+gr_factor_inv = 0.05  # 0.2
+gr_pr_i = 10.1  # 1.1
+MOTION_ACTIVATION_PROBABILITY = 0.005
+MOTION_DEACTIVATION_PROBABILITY = 0.01
+
+# constants regarding biofilm
+START_NUMBER_BACTERIA = 3
+
+# constants regarding visualisation
+WINDOW_SIZE = (500, 750)

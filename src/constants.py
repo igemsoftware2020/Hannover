65fca34914fee5058717afe01fab07d3650b85a3
David <47631890+StudDavid@users.noreply.github.com>
2020-09-07 15:02:49 +0200

    changed a constant for plot testing

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 from pathlib import Path
-import numpy as np
 
 
 class Constants:
@@ -12,20 +11,20 @@ class Constants:
     SOURCE_PATH = ROOT_DIRECTORY / 'src'
 
     # SIMULATION PARAMETERS
-    TIME_STEP = 1 # in seconds
-    NUMBER_ITERATIONS = int(1E4)
+    TIME_STEP = 1  # in seconds
+    NUMBER_ITERATIONS = 250
     # constants B. subtilius
 
-    BSUB_WIDTH = 1    # MICROMETERS https://en.wikipedia.org/wiki/Bacillus_subtilis
+    BSUB_WIDTH = 1  # MICROMETERS https://en.wikipedia.org/wiki/Bacillus_subtilis
     BSUB_LENGTH = 4.9  # MICROMETERS https://en.wikipedia.org/wiki/Bacillus_subtilis
     BSUB_MASS = 10 ** (-15)  # kg
     BSUB_MORTALITY_RATE = 0.01
-    BSUB_CRITICAL_LENGTH = 9  # MICROMETERS
+    BSUB_CRITICAL_LENGTH = 7  # MICROMETERS
     BSUB_MEAN_SPEED = 50  # 50 MICROMETERS / s mean speed
 
-    BSUB_DOUBLING_TIME = 720    # SECONDS DOI: 10.1128/jb.167.1.219-230.1986
+    BSUB_DOUBLING_TIME = 720  # SECONDS DOI: 10.1128/jb.167.1.219-230.1986
     # BSUB_GROWTH_FACTOR = np.log(2) / BSUB_DOUBLING_TIME  # MICROMETERS / s
-    BSUB_GROWTH_FACTOR = 0.05
+    BSUB_GROWTH_FACTOR = 0.3
     gr_factor_inv = 0.05  # 0.2
     gr_pr_i = 10.1  # 1.1
     MOTION_ACTIVATION_PROBABILITY = 0.005

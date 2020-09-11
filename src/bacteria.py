removed wrong rotation of velocity. Still TODO added plot opacity

src/bacteria.py
@@ -153,9 +153,9 @@ class Bacterium:
         Add random angle movement
         """
         if self.at_boundary() == 'X':
-            apply_rotation(self.velocity, rotation_matrix_x(theta=np.pi / 2))
+            apply_rotation(self.velocity, rotation_matrix_x(theta=np.pi))
         elif self.at_boundary() == 'Y':
-            apply_rotation(self.velocity, rotation_matrix_y(theta=np.pi / 2))
+            apply_rotation(self.velocity, rotation_matrix_y(theta=np.pi))
 
         acceleration = self.force / C.BSUB_MASS * 1E-6  # [um / s ** 2]
         # update velocities
@@ -167,21 +167,22 @@ class Bacterium:
             self.velocity[2] = 0
             self.position[2] = 0
 
-        self.velocity = apply_rotation(self.velocity, rotation_matrix_x(self.angle[0]))
-        self.velocity = apply_rotation(self.velocity, rotation_matrix_y(self.angle[1]))
+        # Careful: rotate along bacterium axis
+        # self.velocity = apply_rotation(self.velocity, rotation_matrix_x(self.angle[0]))
+        # self.velocity = apply_rotation(self.velocity, rotation_matrix_y(self.angle[1]))
 
         # add brownian movement
-        self.velocity[0] = self.velocity[0] + random.random()
-        self.velocity[1] = self.velocity[1] + random.random()
-        self.velocity[2] = self.velocity[2] + random.random()
+        self.velocity[0] = self.velocity[0] + random.uniform(-1, 1)  # add random velocity up to 1 um / s
+        self.velocity[1] = self.velocity[1] + random.uniform(-1, 1)
+        self.velocity[2] = self.velocity[2] + random.uniform(-1, 1)
 
         # update angular velocity
         # 3D  instantaneous angular velocity vector w = r x v / |r|^2
         self.velocity_angular = np.cross(self.position, self.velocity) / np.linalg.norm(self.position) ** 2
         # add random rotational velocity
-        self.velocity_angular[0] = self.velocity_angular[0] + random.random() * self.velocity_angular[0]
-        self.velocity_angular[1] = self.velocity_angular[1] + random.random() * self.velocity_angular[1]
-        self.velocity_angular[2] = self.velocity_angular[2] + random.random() * self.velocity_angular[2]
+        self.velocity_angular[0] = self.velocity_angular[0] + random.uniform(- 0.02, 0.02)
+        self.velocity_angular[1] = self.velocity_angular[1] + random.uniform(- 0.02, 0.02)
+        self.velocity_angular[2] = self.velocity_angular[2] + random.uniform(- 0.02, 0.02)
 
     def update_position(self, dt=C.TIME_STEP):
         """ update bacterium position based on velocity """

src/biofilm.py
@@ -88,7 +88,7 @@ class Biofilm(object):
         print("\nSTARTING MODELLING")
         print(f"SIMULATION TIME INTERVAL {duration_in_min} min in steps of {C.TIME_STEP} s.")
         for _ in tqdm.tqdm(range(0, round(duration_in_min * 60 / C.TIME_STEP))):
-            print("STEP ", _)
+
             for bacterium in self.bacteria:
                 # Grow Bacterium
                 bacterium.grow()
@@ -110,9 +110,7 @@ class Biofilm(object):
 
                 if bacterium.moving is True:
                     bacterium.update_velocity()
-                    print("Velocity :", bacterium.velocity)
                     bacterium.update_position()
-                    print("Postition :", bacterium.position)
                 else:
                     bacterium.moving = random.choices([True, False], weights=[0.5, 0.5])[0]
 
@@ -149,12 +147,11 @@ class Biofilm(object):
     def abs_force_lennard_jones_potential(bacterium1: Bacterium, bacterium2: Bacterium):
         """ return interaction term with bacteria in local environment"""
 
-        # TODO: update velocities accordingly
         def lennard_jones_force(r, epsilon, r_min):
             return - epsilon * (12 * (r_min ** 12 / r ** 13) - 12 * (r_min ** 6 / r ** 7))
 
         distance_vector = bacterium1.position - bacterium2.position
-        distance = np.linalg.norm(distance_vector)
+        distance = np.linalg.norm(distance_vector) * 1E6
         repulsive_force = lennard_jones_force(distance, epsilon=C.MAX_CELL_CELL_ADHESION, r_min=bacterium1.width)
         return repulsive_force
 

src/utils.py
@@ -93,7 +93,7 @@ def plot_velocities(data: pd.DataFrame, save_path: Path, save_fig: bool = False)
     means = plot_data.mean(axis=1, skipna=True)
     fig, (ax1, ax2) = plt.subplots(1, 2)
     for bacteria in plot_data:
-        ax1.plot(plot_data.loc[:, bacteria])
+        ax1.plot(plot_data.loc[:, bacteria], '--', alpha=0.3)
 
     ax1.set_title('Velocities')
     ax1.set_xlabel('Step')
@@ -117,7 +117,7 @@ def plot_positions(data: pd.DataFrame, save_path: Path, save_fig: bool = False):
     means = plot_data.mean(axis=1, skipna=True)
     fig, (ax1, ax2) = plt.subplots(1, 2)
     for bacteria in plot_data:
-        ax1.plot(plot_data.loc[:, bacteria])
+        ax1.plot(plot_data.loc[:, bacteria], '--', alpha=0.3)
 
     ax1.set_title('Position')
     ax1.set_xlabel('Step')
@@ -141,7 +141,7 @@ def plot_force(data: pd.DataFrame, save_path: Path, save_fig: bool = False):
     means = plot_data.mean(axis=1, skipna=True)
     fig, (ax1, ax2) = plt.subplots(1, 2)
     for bacteria in plot_data:
-        ax1.plot(plot_data.loc[:, bacteria])
+        ax1.plot(plot_data.loc[:, bacteria], '--', alpha=0.3)
 
     ax1.set_title('Total force')
     ax1.set_xlabel('Step')
@@ -167,8 +167,8 @@ def plot_size(data: pd.DataFrame, save_path: Path, save_fig: bool = False):
     length_means = length_data.mean(axis=1, skipna=True)
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
     for bacteria in width_data:
-        ax1.plot(width_data.loc[:, bacteria])
-        ax2.plot(length_data.loc[:, bacteria.replace('width', 'length')])
+        ax1.plot(width_data.loc[:, bacteria], '--', alpha=0.3)
+        ax2.plot(length_data.loc[:, bacteria.replace('width', 'length')], '--', alpha=0.3)
     ax1.set_title('width')
     ax1.set_xlabel('Step')
     ax1.set_ylabel('width')
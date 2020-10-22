69407c3a07767dae7b0ab86f9e2d57aa9b12f6f8
David <47631890+StudDavid@users.noreply.github.com>
2020-10-22 15:07:49 +0200

    updated version to 1.0.1. fixed issues with plots

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='BiofilmSimulation',
-    version='1.0.0',
+    version='1.0.1',
     description='The model applies methods from molecular dynamics (MD) and takes into account different '
                 'physical and biological effects. '
                 'The software provides great flexibility by enabling the user to switch easily between sets of '
